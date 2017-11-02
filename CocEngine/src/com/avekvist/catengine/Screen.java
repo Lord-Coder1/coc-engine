d3cf809da69f2fcc9b898b9cc9c3b72bda16262b
Lord-Coder1 <shivcava@yahoo.com>
2017-11-02 12:23:24 -0700

    Change To LWJGL
    So you are using swing but as a game engine that may not work well, so you should use it in the game editor instead of the window(should be JOGL or LWJGL 2/3) . Shows the LWJGL2 commit here. OpenGL 3.2 for now.
    Summary: Improved Window Design.

--- CocEngine/src/com/avekvist/catengine/Screen.java
+++ CocEngine/src/com/avekvist/catengine/Screen.java
@@ -1,17 +1,50 @@
 package com.avekvist.catengine;
+import org.lwjgl.LWJGLException;
 
-import javax.swing.JFrame;
+class Screen{
+	//Default dimensions and variables
+	private static int length = 500; 
+	private static int height = 500;
+	private static int FPS_MAX = 30;
+	private static String title = "";
 
-public class Screen extends JFrame {
-
-	private JFrame frame;
-	
-	public Screen(int width, int height, String title) {
-		frame = new JFrame();
-		frame.setSize(width, height);
-		frame.setTitle(title);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public static void setWindowSize(int len, int width){
+		length = len;
+		height = width;
 	}
+
+	public static void setMaxFPS(int fps){
+		FPS_MAX = fps;
+	}
+
+	public static void setTitle(String wintitle){
+		title = wintitle;
+	}
+
+	public static void create(){
+		ContextAttribs openGLattributes = new ContextAttribs(3,2)
+		.withForwardCompatible(true)
+		.withProfileCore(true);
+
+		try{
+			DisplayMode mode = new DisplayMode(width, height);
+			Display.setDisplayMode(mode);
+			Display.create(new PixelFormat(), openGLattributes);
+			Display.setTitle(title);
+		}catch(LWJGLException e){
+			e.printStackTrace();
+		}
+		//setting up the area that can be rendered to the entire Display
+		GL11.glViewport(0,0,length,height);
+	}
+
+	public static void update(){
+		Display.sync(FPS_MAX);
+		Display.update();
+	}
+
+	public static void destroy(){
+		Display.destroy();
+	}
+
 }
